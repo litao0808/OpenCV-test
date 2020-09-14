fcb926bad4a878d10c3e21880534b2b14dbb4ddd
litao0808 <58644816+litao0808@users.noreply.github.com>
2020-09-14 08:59:29 +0800

    Add files via upload

---
+++ test.cpp
@@ -0,0 +1,17 @@
+﻿
+
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+using namespace std;
+using namespace cv;
+
+int main()
+{
+	Mat img = imread("lena.jpg");
+	imshow("le",img);
+	waitKey(0);
+    cout << "Hello World!\n";
+}
+
+
